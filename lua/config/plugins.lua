85df0ddbb44fe498a61214d1b827b24a968dec7b
Demenech <joaommdemenech@gmail.com>
2024-01-19 16:38:46 -0300

    Fix issue when formatting buffer

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -96,5 +96,12 @@ return {
         config = function()
           require("plugins.prettier")
         end
+    },
+    {
+	"L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
     }
 }
